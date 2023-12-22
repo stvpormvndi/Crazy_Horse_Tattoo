60f8ac2c0e0364e6c962f680a88380a0d42e5712
stvpormvndi <f.lumachini@gmail.com>
2023-12-22 10:54:14 +0100

    Some changes are needed

--- script.js
+++ script.js
@@ -1,74 +1,137 @@
 /* Script for the toggle button. */
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar")[0];
+const toggleBars = document.querySelectorAll(".bar");
 
 toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle('active');
+  navbarLinks.classList.toggle("active");
+  navbarLinks.classList.toggle("inactive");
+  toggleBars[1].classList.toggle("x-form");
+  toggleBars[0].classList.toggle("drop-right");
+  toggleBars[2].classList.toggle("drop-left");
 });
 
 /* A slider class */
 
 class simpleImageSlider {
-    /* A simple slider with */
-    constructor(slideLeftButton, slideRightButton, leftSideImage, centralImage, rightSideImage, imageUrlsArray){
-        this.slideLeftButton = slideLeftButton;
-        this.slideRightButton = slideRightButton;
-        this.leftSideImage = leftSideImage;
-        this.centralImage = centralImage;
-        this.rightSideImage = rightSideImage;
-        this.imageUrlsArray = imageUrlsArray;
-        this.leftSideImageIndex = 0;
-        this.centralImageIndex = 1;
-        this.rightSideImageIndex = 2;
-        this.slideLeftButton.addEventListener("click", () => {
-            /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`); */
-            --this.leftSideImageIndex;
-            --this.centralImageIndex;
-            --this.rightSideImageIndex;
-            if (this.leftSideImageIndex < 0) {
-                this.leftSideImageIndex = this.imageUrlsArray.length - 1;
-            };
-            if (this.centralImageIndex < 0) {
-                this.centralImageIndex = this.imageUrlsArray.length - 1;
-            };
-            if (this.rightSideImageIndex < 0) {
-                this.rightSideImageIndex = this.imageUrlsArray.length -1;
-            };
-            /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`); */
-            this.leftSideImage.setAttribute("src", this.imageUrlsArray[this.leftSideImageIndex]);
-            this.centralImage.setAttribute("src", this.imageUrlsArray[this.centralImageIndex]);
-            this.rightSideImage.setAttribute("src", this.imageUrlsArray[this.rightSideImageIndex]);
-        });
-        this.slideRightButton.addEventListener("click", () => {
-            /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`);
+  /* A simple slider with */
+  constructor(
+    slideLeftButton,
+    slideRightButton,
+    leftSideImage,
+    centralImage,
+    rightSideImage,
+    imageUrlsArray
+  ) {
+    this.slideLeftButton = slideLeftButton;
+    this.slideRightButton = slideRightButton;
+    this.leftSideImage = leftSideImage;
+    this.centralImage = centralImage;
+    this.rightSideImage = rightSideImage;
+    this.imageUrlsArray = imageUrlsArray;
+    this.leftSideImageIndex = 0;
+    this.centralImageIndex = 1;
+    this.rightSideImageIndex = 2;
+    this.slideLeftButton.addEventListener("click", () => {
+      /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`); */
+      --this.leftSideImageIndex;
+      --this.centralImageIndex;
+      --this.rightSideImageIndex;
+      if (this.leftSideImageIndex < 0) {
+        this.leftSideImageIndex = this.imageUrlsArray.length - 1;
+      }
+      if (this.centralImageIndex < 0) {
+        this.centralImageIndex = this.imageUrlsArray.length - 1;
+      }
+      if (this.rightSideImageIndex < 0) {
+        this.rightSideImageIndex = this.imageUrlsArray.length - 1;
+      }
+      /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`); */
+      this.leftSideImage.setAttribute(
+        "src",
+        this.imageUrlsArray[this.leftSideImageIndex]
+      );
+      this.centralImage.setAttribute(
+        "src",
+        this.imageUrlsArray[this.centralImageIndex]
+      );
+      this.rightSideImage.setAttribute(
+        "src",
+        this.imageUrlsArray[this.rightSideImageIndex]
+      );
+    });
+    this.slideRightButton.addEventListener("click", () => {
+      /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`);
             console.log(this.imageUrlsArray.length); */
-            ++this.leftSideImageIndex;
-            ++this.centralImageIndex;
-            ++this.rightSideImageIndex;
-            if (this.leftSideImageIndex == this.imageUrlsArray.length) {
-                this.leftSideImageIndex = 0;
-            };
-            if (this.centralImageIndex == this.imageUrlsArray.length) {
-                this.centralImageIndex = 0;
-            };
-            if (this.rightSideImageIndex == this.imageUrlsArray.length) {
-                this.rightSideImageIndex = 0;
-            };
-            /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`); */
-            this.leftSideImage.setAttribute("src", this.imageUrlsArray[this.leftSideImageIndex]);
-            this.centralImage.setAttribute("src", this.imageUrlsArray[this.centralImageIndex]);
-            this.rightSideImage.setAttribute("src", this.imageUrlsArray[this.rightSideImageIndex]);
-        });
-    }
+      ++this.leftSideImageIndex;
+      ++this.centralImageIndex;
+      ++this.rightSideImageIndex;
+      if (this.leftSideImageIndex == this.imageUrlsArray.length) {
+        this.leftSideImageIndex = 0;
+      }
+      if (this.centralImageIndex == this.imageUrlsArray.length) {
+        this.centralImageIndex = 0;
+      }
+      if (this.rightSideImageIndex == this.imageUrlsArray.length) {
+        this.rightSideImageIndex = 0;
+      }
+      /* console.log(`leftSideImageIndex: ${this.leftSideImageIndex}  centralImageIndex: ${this.centralImageIndex}  rightSideImageIndex: ${this.rightSideImageIndex}`); */
+      this.leftSideImage.setAttribute(
+        "src",
+        this.imageUrlsArray[this.leftSideImageIndex]
+      );
+      this.centralImage.setAttribute(
+        "src",
+        this.imageUrlsArray[this.centralImageIndex]
+      );
+      this.rightSideImage.setAttribute(
+        "src",
+        this.imageUrlsArray[this.rightSideImageIndex]
+      );
+    });
+  }
 }
 
 const leftPiercingButton = document.querySelector(".left_side_button");
 const rightPiercingButton = document.querySelector(".right_side_button");
-const piercingUrls = ["images/piercings/piercing_1.jpg" ,"images/piercings/piercing_2.jpg", "images/piercings/piercing_3.jpg", "images/piercings/piercing_4.jpg"];
-const leftPiercingImage = document.querySelector(".immagine_piercing:first-child");
-const centralPiercingImage = document.querySelector(".immagine_piercing:nth-child(2)");
-const rightPiercingImage = document.querySelector(".immagine_piercing:last-child");
+const piercingUrls = [
+  "images/piercings/piercing_1.jpg",
+  "images/piercings/piercing_2.jpg",
+  "images/piercings/piercing_3.jpg",
+  "images/piercings/piercing_4.jpg",
+];
+const leftPiercingImage = document.querySelector(
+  ".immagine_piercing:first-child"
+);
+const centralPiercingImage = document.querySelector(
+  ".immagine_piercing:nth-child(2)"
+);
+const rightPiercingImage = document.querySelector(
+  ".immagine_piercing:last-child"
+);
 
-let piercingSlider = new simpleImageSlider(leftPiercingButton, rightPiercingButton, leftPiercingImage, centralPiercingImage, rightPiercingImage, piercingUrls);
+let piercingSlider = new simpleImageSlider(
+  leftPiercingButton,
+  rightPiercingButton,
+  leftPiercingImage,
+  centralPiercingImage,
+  rightPiercingImage,
+  piercingUrls
+);
 
+/* The image slider */
+let slideIndex = 0;
+automaticSlideshow();
+setInterval(automaticSlideshow, 2500);
 
+function automaticSlideshow() {
+  let slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+  slideIndex++;
+  if (slideIndex === slides.length) {
+    slideIndex = 0;
+  }
+}
